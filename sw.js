8936e8f8f32d1a36be47fdff8b28af5272cd9528
HibbanNurrizki <hibbannurrizki25@upi.edu>
2026-05-03 07:41:00 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'elp-english-kit-v1';
+const urlsToCache = [
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names => 
+      Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request).then(fetchRes => {
+        if (!fetchRes || fetchRes.status !== 200) return fetchRes;
+        const resClone = fetchRes.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, resClone));
+        return fetchRes;
+      }).catch(() => caches.match('/index.html'));
+    })
+  );
+});
